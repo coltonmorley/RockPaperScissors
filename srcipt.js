10bcae42db20cb8c691c9fdbd49255895d77c3da
Colton Morley <ColtonMorley42@gmail.com>
2023-02-01 21:36:03 -0800

    Final Commit

--- srcipt.js
+++ srcipt.js
@@ -1,128 +1,139 @@
-// console.log("Test that mufffalkkek")
-
-// let gameComplete = false;
-
-// let test = 0;
-// test = getRNG();
+userScore = 0;
+compScore = 0;
 
 
-// console.log(test)
-
-// console.log("Welcome to Rock Paper Scissors Simulator");
-// console.log("\nThis will be a best of five battle!\n\n");
-// console.log("Type either Rock, Paper, or Scissors here:");
-
-
-
-// while(gameComplete){
-//     let userScore = 0;
-//     let compScore = 0;
-//     console.log("fucked");
-// }
-
-
-
-// function getRNG() {
-//     return Math.floor(Math.random() * 3 );
-// }
-
-// //U represents User and C represents computer
-// //Returns 1 if user wins round, 0 if computer wins round, and -1 if tie
-// function winner(u, c) {
-//     if (u==c){return -1;} 
-//     else if (u == 0){
-//         if(c == 2) {return 1;}
-//         else {return 0;};
-//     }
-//     else if (u == 1) {
-//         if (c == 0) {return 1;}
-//         else {return 0;};
-//     }
-//     else if (u == 2){
-//         if (c == 1) {return 1;}
-//         else {return 0;};
-//     }
-//     //Something must've gone wrong if we get here
-//     else {return 200;};
-
-// }
-
-// let roundCnt = 0;
-// let uScore = 0;
-// let cScore = 0;
-
-// function getPick () {
-//     document.getElementById("winLoss").innerHTML = ``;
-//     let x = document.getElementById("userIn").value;
-    
-//     console.log(x.toUpperCase())
-//     document.getElementById("meta").innerHTML =  `You chose ${x}`;
-//     userChoice = convertToInt(x.toUpperCase());
-//     if (userChoice == -1){
-//         console.log("Invalid, please enter rock paper or scissors");
-//         return -1;
-//     }
-//     compChoice = getRNG();
-//     result = winner(userChoice, compChoice);
-//     //console.log(`User: ${userChoice}, Comp: ${compChoice}, winner: ${result}`);
-//     if(result == -1){
-//         document.getElementById("battleResult").innerHTML =  `You both had ${convertToChar(compChoice)} its a draw! `;
-//         console.log(`You both had ${convertToChar(compChoice)} its a draw! `)
-//     }
-//     else if (result){
-//         document.getElementById("battleResult").innerHTML =`Round Won! ${convertToChar(userChoice)} beats ${convertToChar(compChoice)}!`;
-//         console.log(`Round Won! ${convertToChar(userChoice)} beats ${convertToChar(compChoice)}!`);
-//         uScore++;
-//     }
-//     else {
-//         document.getElementById("battleResult").innerHTML = `Round Lost! ${convertToChar(userChoice)} does not beat ${convertToChar(compChoice)}!`
-//         console.log(`Round Lost! ${convertToChar(userChoice)} does not beat ${convertToChar(compChoice)}!`);
-//         cScore++;
-//     }
-//     roundCnt++;
-//     updateHTML(uScore, cScore, roundCnt);
-
-//     if(uScore == 5) {
-//         document.getElementById("winLoss").innerHTML = `You WIN! Enter a new value and submit to start a new game`;
-//         roundCnt = 0;
-//         uScore = 0;
-//         cScore = 0;
-//     } 
-//     else if (cScore == 5){
-//         document.getElementById("winLoss").innerHTML = `You lost, enter a value and submit to try again!`;
-//         roundCnt = 0;
-//         uScore = 0;
-//         cScore = 0;
-//     }
-// }   
+function playRound(player){
+    computer = getRand();
+    playClipart(player, computer);
+    pWin = winner(player, computer);
+    winLossBG(pWin);
+    if(pWin == 1) {userScore++};
+    if(pWin == 0) {compScore++};
+    //Prevent button spam
+    disableButtons();
+    setTimeout(() => {  nextRound(); }, 3000);
+    if(gameOver()){
+        setTimeout(() => {  endGame(pWin); }, 3000);
+    }
+}
 
 
+function getRand(){
+    return Math.floor(Math.random() * 3 );
+}
 
-// function convertToInt (str) {
-//     if(str == "ROCK"){
-//         return 0;
-//     } else if (str == "PAPER"){
-//         return 1;
-//     } else if (str == "SCISSORS"){
-//         return 2;
-//     }else {return -1;}
-//     }
 
-// function convertToChar (int) {
-//     if(int == 0){
-//         return "Rock";
-//     } else if (int == 1){            
-//         return "Paper";
-//     } else if (int = 2){
-//         return "Scissors";        
-//     }else {
-//         console.log(`WE GOT AN ERRRRRRRORRRRRRR ${int}`)
-//         return -1;}
-//     }
+//Returns 1 if user wins round, 0 if computer wins round, and -1 if tie
+function winner(u, c) {
+    if (u==c){return -1;} 
+    else if (u == 0){
+        if(c == 2) {return 1;}
+        else {return 0;};
+    }
+    else if (u == 1) {
+        if (c == 0) {return 1;}
+        else {return 0;};
+    }
+    else if (u == 2){
+        if (c == 1) {return 1;}
+        else {return 0;};
+    }
+    //Something must've gone wrong if we get here
+    else {return 200;};
 
-// function updateHTML (u, c, r){
-//     document.getElementById("round").innerHTML = `After round ${r}`;
-//     document.getElementById("uScore").innerHTML = `You have ${u} wins!`;
-//     document.getElementById("cScore").innerHTML = `The computer has ${c} wins!`;
+}
 
-// }
+
+function playClipart(player, computer) {
+    us = document.getElementById('userScore');
+    cs = document.getElementById('compScore');
+    if(player == 0){
+        us.innerHTML = '<img class = clipartSB src =\'./images/rock.png\' alt = "Rock clipart"></img>';
+    }
+    else if (player == 1){
+        us.innerHTML = '<img class = clipartSB src =\'./images/paper.png\' alt = "Paper clipart"></img>';
+    }
+    else if (player == 2){
+        us.innerHTML = '<img class = clipartSB src =\'./images/scissors.png\' alt = "Scissors clipart"></img>';
+    }
+    //Set Computer scoreboard clipart
+    if(computer == 0){
+        cs.innerHTML = '<img class = clipartSB src =\'./images/rock.png\' alt = "Rock clipart"></img>';
+    }
+    else if (computer == 1){
+        cs.innerHTML = '<img class = clipartSB src =\'./images/paper.png\' alt = "Paper clipart"></img>';
+    }
+    else if (computer == 2){
+        cs.innerHTML = '<img class = clipartSB src =\'./images/scissors.png\' alt = "Scissors clipart"></img>';
+    }
+}
+
+
+//Highlight scoreboard based on who won, 1 = user win, 0 = comp win, -1 if tie
+function winLossBG(playerWin){
+    player = document.querySelector('.score');
+    computer = document.querySelector('.score:nth-child(2)');
+    if(playerWin == 1){
+        player.classList.add('winner');
+        computer.classList.add('loser');
+    }
+    else if(playerWin == 0){
+        computer.classList.add('winner');
+        player.classList.add('loser');
+    }
+    else {
+        computer.classList.add('tie');
+        player.classList.add('tie');
+    }
+}
+
+function nextRound(){
+    player = document.querySelector('.score');
+    computer = document.querySelector('.score:nth-child(2)');
+
+    player.classList.remove('loser');
+    player.classList.remove('winner');
+    player.classList.remove('tie');
+    computer.classList.remove('loser');
+    computer.classList.remove('winner');
+    computer.classList.remove('tie');
+
+    us = document.getElementById('userScore');
+    cs = document.getElementById('compScore');
+
+    us.textContent = userScore;
+    cs.textContent = compScore;
+
+    buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {button.disabled = false});
+}
+
+
+function gameOver(){
+    if(userScore >= 5 || compScore >= 5) return 1;
+    return 0;
+}
+
+function endGame(){
+    disableButtons();
+    container = document.querySelector('.scoreboard');
+    container.classList.add('updateFlex');
+    if(pWin){
+        container.innerHTML= '<div class = \'score\'>You bested the computer! Refresh to see if you can do it again!</div>';
+    }
+    else{
+        container.innerHTML= '<div class = \'score\'>You Lost! Refresh to try again!</div>';
+    }
+
+
+}
+
+
+function disableButtons(){
+    buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {button.disabled = true});
+}
+
+function refreshPage(){
+    window.location.reload();
+}
